aba418e303c91b0ad2300065bb97950f7d119f2e
mehmet yalcinkaya <mehmeasda@fama.com>
2020-01-30 12:42:38 +0300

    Create API

---
+++ DataTransfer/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DataTransfer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataTransfer.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
